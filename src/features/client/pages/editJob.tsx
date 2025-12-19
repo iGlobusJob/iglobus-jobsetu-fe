52e7fa2d0ed2e27b97bd800156328ff75ef2c10b
YadagiriTalada <yadagiritalada@gmail.com>
2025-12-19 18:02:48 +0530

    Fixed build issues

--- src/features/client/pages/editJob.tsx
+++ src/features/client/pages/editJob.tsx
@@ -1,10 +1,7 @@
-import { useParams } from 'react-router-dom';
-
 import { JobPostForm } from '../components/job/addJob';
 
 export const EditJobPage = () => {
-  const { jobId } = useParams<{ jobId: string }>();
-  return <JobPostForm jobId={jobId} />;
+  return <JobPostForm />;
 };
 
 export default EditJobPage;
