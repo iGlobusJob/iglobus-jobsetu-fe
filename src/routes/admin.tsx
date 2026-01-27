ccaa27fc7de8b4ac10140881e76740005301099a
Syed Farhan <syedfarhan596@gmail.com>
2026-01-27 23:28:01 +0530

    update candidate UI in admin and recruiter

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -2,11 +2,12 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AddAdminPage from '@/features/admin/pages/AddAdminPage';
 import AnalyticsDashboard from '@/features/admin/pages/AnalyticsDashboard';
-import CandidateDashboard from '@/features/admin/pages/CandidateDashboard';
 import ClientDetailsPage from '@/features/admin/pages/ClientDetailsPage';
 import RecruiterDashboard from '@/features/admin/pages/RecruiterDashboard';
 import ClientDashboard from '@/features/admin/pages/dashboard';
 import AllJobs from '@/features/admin/pages/jobs';
+import CandidateDetailPage from '@/features/recruiter/pages/candidateDetail';
+import CandidatesPage from '@/features/recruiter/pages/candidates';
 
 import { RoleRoute } from './guards';
 
@@ -19,11 +20,16 @@ export const AdminRoutes = () => {
         <Route path="dashboard" element={<AnalyticsDashboard />} />
         <Route path="clients" element={<ClientDashboard />} />
         <Route path="clients/:clientId" element={<ClientDetailsPage />} />
-        <Route path="candidates" element={<CandidateDashboard />} />
+        <Route path="candidates" element={<CandidatesPage />} />
         <Route path="recruiters" element={<RecruiterDashboard />} />
         {/* Add more admin routes here */}
         <Route path="add-admin" element={<AddAdminPage />} />
         <Route path="all-jobs" element={<AllJobs />} />
+
+        <Route
+          path="/:candidateId/candidate"
+          element={<CandidateDetailPage />}
+        />
         {/* Default redirect */}
         <Route index element={<Navigate to="dashboard" replace />} />
       </Route>
--- src/routes/config/recruiterPath.ts
+++ src/routes/config/recruiterPath.ts
@@ -2,4 +2,5 @@ export const RECRUITER_PATHS = {
   BASE: '/recruiter',
   JOBS: '/recruiter/jobs',
   JOB_DETAILS: (id: string) => `/recruiter/${id}/job-details`,
+  CANDIDATE_DETAILS: (id: string) => `/recruiter/${id}/candidate`,
 } as const;
